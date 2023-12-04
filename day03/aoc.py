1e6ff3d57c99d7474cbdb3b4798cd6d536ea881b
Andreas Hansen <andreas.hansen@accenture.com>
2023-12-04 14:16:36 +0100

    wip

--- day03/aoc.py
+++ day03/aoc.py
@@ -5,18 +5,20 @@ def part1():
     with open('test.txt') as f:
         lines=f.readlines()
         hashmap = {}
-        print("potato")
-        # Adding key-value pairs to the hashmap
-        hashmap["key1"] = "value1"
-        hashmap["key2"] = "value2"
-        hashmap["key3"] = "value3"
-
-        # Accessing values using keys
-        print(hashmap["key1"])  # Output: value1
-        print(hashmap["key2"])  # Output: value2
-        print(hashmap["key3"])  # Output: value3
-        #4361
-
+        y=0
+        #load data into map
+        for line in lines:
+            line=line.strip()
+            splitted= [*line]
+            x=0
+            for split in splitted:
+                coord=f"{y},{x}"
+                hashmap[coord]=split
+                x=x+1
+            y=y+1
+        
+        
+        print(hashmap)
 
 def part2():
     with open('input.txt') as f:
@@ -28,3 +30,4 @@ if part == "part1":
     part1()
 if part == "part2":
     part2()
+part1()
