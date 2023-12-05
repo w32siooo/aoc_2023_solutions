00933828beccbe6bc0b227b72a89efa1fdf14dc2
Andreas Hansen <andreas.hansen@accenture.com>
2023-12-05 15:21:42 +0100

    wip

--- day05/aoc.py
+++ day05/aoc.py
@@ -3,101 +3,73 @@ import os
 number_extract_pattern = "\\d+"
 
 def part1():
-    with open('test.txt') as f:
-        lines=f.readlines()
-        total_score=0
-        seed_to_soil=False
-        soil_to_fertilizer=False
-        f_2_w=False
-        w_2_l=False
-        l_2_t=False
-        t_2_h=False
-        h_2_l=False
-        seed_to_soil_map={}
-        soil_to_fertilizer_map={}
-        f_2_w_map={}
-        w_2_l_map={}
-        l_2_t_map={}
-        t_2_h_map={}
-        h_2_l_map={}
-        for line in lines:
-            if line.startswith("\n"):
-                seed_to_soil=False
-                soil_to_fertilizer=False
-                f_2_w=False
-                w_2_l=False
-                l_2_t=False
-                t_2_h=False
-                h_2_l=False
-                tmp=None
-            if line.startswith("seeds"):
-                seeds=line.split(":")[1].strip().split(" ")
-                print(f"seeds: {seeds}")
-            if seed_to_soil:
-                tmp=line.strip().split(" ")
-                print(f"seed to soil: {tmp}")
-                for i in range (int(tmp[2])):
-                    source=int(tmp[1])+i
-                    dest=int(tmp[0])+i
-                    seed_to_soil_map[source]=dest
-            if soil_to_fertilizer:
-                tmp=line.strip().split(" ")
-                print(f"soil_to_fertilizer: {tmp}")
-            if f_2_w:
-                tmp=line.strip().split(" ")
-                print(f"fert 2 water: {tmp}")
-            if w_2_l:
-                tmp=line.strip().split(" ")
-                print(f"w_2_l: {tmp}")
-            if l_2_t:
-                tmp=line.strip().split(" ")
-                print(f"seed to soil: {tmp}")
-            if t_2_h:
-                tmp=line.strip().split(" ")
-                print(f"t_2_h_map: {tmp}")
-            if h_2_l:
-                tmp=line.strip().split(" ")
-                print(f"h_2_l_map: {tmp}")
-            if line.startswith("seed-to-soil map"):
-                seed_to_soil=True
-            if line.startswith("soil-to-fertilizer map"):
-                soil_to_fertilizer=True
-            if line.startswith("fertilizer-to-water map"):
-                f_2_w=True
-            if line.startswith("water-to-light map"):
-                w_2_l=True
-            if line.startswith("light-to-temperature map"):
-                l_2_t=True
-            if line.startswith("temperature-to-humidity map"):
-                t_2_h=True
-            if line.startswith("humidity-to-location map"):
-                h_2_l=True
-            print(seed_to_soil_map)
-            
-            
-# Consider again the example seed-to-soil map:
-
-# 50 98 2
-# 52 50 48
-#The first line has a destination range start of 50, a source range start of 98, and a range length of 2. 
-# This line means that the source range starts at 98 and contains two values: 98 and 99.
-#  The destination range is the same length, but it starts at 50, so its two values are 50 and 51.
-#  With this information, you know that seed number 98 corresponds to soil number 50 and that seed number 99 corresponds to soil number 51.
-
-
-
-#a destination range start of 50, a source range start of 98, and a range length
-#Any source numbers that aren't mapped correspond to the same destination number. So, seed number 10 corresponds to soil number 10.
-
-
-def part2():
     with open('input.txt') as f:
         lines=f.readlines()
         total_score=0
-        print(total_score)
+        maps_list=[]
+        range_map_list=[]
+        parse=False
+        newMap={}
+        range_map={}
+        #key = newMap
+        #value
+        seeds=""
+        lines.append("/n")
+        for line in lines:
+            if len(re.findall(number_extract_pattern,line))>0 and not line.startswith("seeds:"):
+                tmp=line.strip().split(" ")
+                range_key = (int(tmp[2]))
+                source=int(tmp[1])
+                dest=int(tmp[0])
+                newMap[source]=dest
+                range_map[counter]=range_key
+                counter=counter+1
+            else:
+                range_map_list.append(range_map)
+                maps_list.append(newMap)
+                range_map={}
+                newMap={}
+                counter=0
+            if line.startswith("seeds"):
+                seeds=line.split(":")[1].strip().split(" ")
+
+        final_locs=[]
+        for seed in seeds:
+            seed=int(seed)
+
+            for map_index in range (len(maps_list)):
+                curr_map = maps_list[map_index]
+                #print(curr_map)
+                cnt=0
+                a_match=True
+                for source in curr_map:
+                    dest=curr_map[source]
+                    rng=range_map_list[map_index][cnt]
+                    #print(f"src: {source} dest: {dest} range {rng}")
+                    min_rng=source
+                    max_rng=source+(rng-1)
+                    
+                    if min_rng <= seed <= max_rng:
+                        seed=dest-(source-seed)
+                        a_match=False
+                        break
+                    if a_match != True:
+                        continue
+                    
+                    cnt=cnt+1
+                
+
+            final_locs.append(seed)
+
+        print(min(final_locs))
+
+            
+
+def part2():
+    with open('test.txt') as f:
+        print("not sure yet")
 part = os.environ.get('part')
 if part == "part1":
     part1()
 if part == "part2":
     part2()
-part1()
